50fcba74a7b1f6836535fd90f8c050a269e4e978
Mahesh-Bharati <maheshbharati123@gmail.com>
2024-04-16 18:45:17 +0530

    Connnecting to the json file using express and use that data in react

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+//import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/data');
+      setData(response.data);
+      console.log(response.data)
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   return (
-    <>
-      <h1>Home</h1>
-    </>
-  )
+    <div className="App">
+      <h1>JSON Data</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
