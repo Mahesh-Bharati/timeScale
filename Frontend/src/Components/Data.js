50f5b2ea3dddb518f2897e069f7495002cc45a02
Mahesh-Bharati <maheshbharati123@gmail.com>
2024-04-17 10:38:43 +0530

    displaying the time scale with span of 1 hr

--- Frontend/src/Components/Data.js
+++ Frontend/src/Components/Data.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Data() {
   const [data, setData] = useState([]);
@@ -11,26 +10,32 @@ function Data() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/data');
+      const response = await axios.get("http://localhost:5000/api/data");
       setData(response.data);
-      console.log(response.data)
+      console.log(response.data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
   return (
     <div className="App">
-        <div style={{ paddingTop: '45px' }}>Cycle Status</div>
-
+      <div style={{ paddingTop: "45px" }}>
+        <h2>Cycle Status</h2>
+      </div>
       <div className="timeline">
         {data.map((item, index) => (
           <div
             key={index}
-            className={`timeline-item ${item.machine_status === 1 ? 'yellow' : 'green'}`}
+            className={`timeline-item ${
+              item.machine_status === 1 ? "yellow" : item.machine_status === 0 ? "green" : "red"
+            }`}
           ></div>
         ))}
       </div>
+
+
+
     </div>
   );
 }
